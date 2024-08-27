c8edcf88c4b553d3e12de4c00458c5c0c4e43bb6
MariyumImran <mariyumimran247@gmail.com>
2024-08-27 18:21:42 -0300

    GuessTheNumber bugFixed StopLoop

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,18 +1,19 @@
 import random
 
 min:int = 1
-max:int = 100
+max:int = 1000
 num:int = 0
 num = random.randint(min, max)
 
 
 
 while True:
-    guess = int(input("Guess a number between 1 to 100 (or press cntr + c to e "))
+    guess = int(input("Guess a number between 1 to 1000:    "))
     #guess2 = "int(input("Guess a number between 1 to 100:    "))"
 
     if guess == num:
             print("Congratulations! You guessed the correct number.")
+            break
     elif guess < num:
             print ("Too low. Try again.")
             #print("Guess a number between 1 to 100:    ")
